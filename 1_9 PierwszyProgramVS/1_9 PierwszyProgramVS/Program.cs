d65fdeee55132c1bc2522778de4cf0d376d61c00
Mateusz <szczepan.mateusz@gmail.com>
2020-04-14 21:38:17 +0200

    1_9 PierwszyProgramVS

---
+++ 1_9 PierwszyProgramVS/1_9 PierwszyProgramVS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace _1_9_PierwszyProgramVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           String imie = args[0];
+            Console.WriteLine("Witaj"+imie);
+            
+        }
+    }
+}
